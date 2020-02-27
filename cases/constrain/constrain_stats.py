e7eafeaf13b65031af4f62e31053fa7e7acd2405
gwitzke <gwitzke95@gmail.com>
2020-02-27 08:14:57 -0500

    Stats file for constrain

--- cases/constrain/constrain_stats.py
+++ cases/constrain/constrain_stats.py
@@ -6,7 +6,7 @@ Constrain CAO statistics
 import numpy as np
 import struct
 import netCDF4
-import matplotlib as plt
+from pylab import *
 
 nt = 12
 plotens = False
@@ -24,41 +24,75 @@ def plotstats(name,line):
     areaht = stats.groups["default"].variables["areah"][:,:]
 
     #Assigning Time Series Variable names
-    Cloud_cover
-    LPW
-    RWP
-    SHF
-    LHF
-    SPF
-    Entrain
+    LWPt = stats.groups["thermo"].variables["ql_path"][:]
+    #RWP(rain water path) =
+    SHFt = stats.groups["default"].variables["thl_diff"][:,:]
+    LHFt = stats.groups["default"].variables["qt_diff"][:,:]
+    #SPF(surface precipatation flux)
+    #Entrainment
 
     #Assigning Thermodynamic variable names
-    th_l 
-    qt 
+    thl  = stats.groups["default"].variables["thl"][:,:]
+    qtt  = stats.groups["default"].variables["qt"][:,:]*1000
+    qlt  = stats.groups["thermo"].variables["ql"][:,:]*1000
+
     
     #Assigning Wind Component variable names
-    u 
-    v
-    w
+    u  = stats.groups["default"].variables["u"][:,:]
+    u2 = stats.groups["default"].variables["u_2"][:,:]
+    v  = stats.groups["default"].variables["v"][:,:]
+    v2 = stats.groups["default"].variables["v_2"][:,:]
+
+    
 
     #Caluclations
-    end = t.size
-    start = t.size -nt
+    end   = t.size
+    start = t.size - nt
 
     #Time Series Calculations
+    Clouds = np.mean(areat[start:end,:], 0)
+    #SHF    = np.sum(areat[start:end,:]*SHFt[start:end,:], 0) / np.sum(areat[start:end,:], 0)
+    #LHF    = np.sum(areat[start:end,:]*LHFt[start:end,:], 0) / np.sum(areat[start:end,:], 0)
+    LWP    = np.sum(areat[start:end]*LWPt[start:end], 0) / np.sum(areat[start:end], 0)
+
 
 
     #Thermodynamic Profile Calculations
+    th_l = np.sum(areat[start:end,:]*thl[start:end,:], 0) / np.sum(areat[start:end,:], 0)
+    qt   = np.sum(areat[start:end,:]*qtt[start:end,:], 0) / np.sum(areat[start:end,:], 0)
+    ql   = np.sum(areat[start:end,:]*qlt[start:end,:], 0) / np.sum(areat[start:end,:], 0)
 
     #Plotting
     f = 1
 
     #Plots of Time Series
+    figure(f)
+    if(plotens):
+        for n in range(start,end):
+            plot(areat[n,:],z)
+    plot(Clouds, z, line, label=name)
+    title("Cloud Coverage")
+
+    f += 1
+    figure(f)
+    if(plotens):
+        for n in range(start,end):
+            plot(LWPt[:],t)
+    plot(LWP, t, line, label=name)
+    title("Liqiud Water Path")
+
 
     #Plots of Thermodynamic profiles
+    f += 1
+    figure(f)
+    if(plotens):
+        for n in range(start, end):
+            plot(thl[n,:], z)
+    plot(th_l, z, line, label=name)
+    title("Liquid Potential Temperature")
 
-plotstats()
-plt.show()
+plotstats('default', 'k-')
+show()
 
 
 
